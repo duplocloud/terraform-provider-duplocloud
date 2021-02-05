72280455b7e7a474bca5f4a72282aad9f5ec6d84
Joe Khoobyar <fourheads@gmail.com>
2021-02-04 23:45:19 -0500

    go fmt

--- duplosdk/types.go
+++ duplosdk/types.go
@@ -3,8 +3,8 @@ package duplosdk
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
- 	"encoding/json"
- 	"log"
+	"encoding/json"
+	"log"
 )
 
 // Generic value holder
@@ -14,7 +14,7 @@ type DuploValue struct {
 
 // Generic name holder
 type DuploName struct {
-	Name  string `json:"Name,omitempty"`
+	Name string `json:"Name,omitempty"`
 }
 
 // Generic key value holder
@@ -30,12 +30,12 @@ type DuploNameValue struct {
 }
 
 func duploKeyValueFromState(fieldName string, d *schema.ResourceData) *[]DuploKeyValue {
-    var ary []DuploKeyValue
+	var ary []DuploKeyValue
 
 	kvs := d.Get(fieldName).([]interface{})
 	if len(kvs) > 0 {
-        log.Printf("[TRACE] duploKeyValueFromState ********: found %s", fieldName)
-        ary = make([]DuploKeyValue, 0, len(kvs))
+		log.Printf("[TRACE] duploKeyValueFromState ********: found %s", fieldName)
+		ary = make([]DuploKeyValue, 0, len(kvs))
 		for _, raw := range kvs {
 			kv := raw.(map[string]interface{})
 			ary = append(ary, DuploKeyValue{
@@ -45,13 +45,13 @@ func duploKeyValueFromState(fieldName string, d *schema.ResourceData) *[]DuploKe
 		}
 	}
 
-    return &ary
+	return &ary
 }
 
 func duploKeyValueToState(fieldName string, duploObjects *[]DuploKeyValue) []interface{} {
 	if duploObjects != nil {
-        input, _ := json.Marshal(&duploObjects)
-        log.Printf("[TRACE] duploKeyValueToState[%s] ******** INPUT <= %s", fieldName, input)
+		input, _ := json.Marshal(&duploObjects)
+		log.Printf("[TRACE] duploKeyValueToState[%s] ******** INPUT <= %s", fieldName, input)
 
 		output := make([]interface{}, len(*duploObjects), len(*duploObjects))
 		for i, duploObject := range *duploObjects {
@@ -65,6 +65,6 @@ func duploKeyValueToState(fieldName string, duploObjects *[]DuploKeyValue) []int
 		return output
 	}
 
-    log.Printf("[TRACE] duploKeyValueToState[%s] ******** EMPTY INPUT", fieldName)
+	log.Printf("[TRACE] duploKeyValueToState[%s] ******** EMPTY INPUT", fieldName)
 	return make([]interface{}, 0)
 }
