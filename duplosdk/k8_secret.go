0a97c84b31b7dc3131072acfa6e4dffa126db87d
Joe Khoobyar <joe@duplocloud.net>
2021-02-15 18:44:11 -0500

    more go fmt and go-lint clean up

--- duplosdk/k8_secret.go
+++ duplosdk/k8_secret.go
@@ -3,39 +3,40 @@ package duplosdk
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"log"
 	"net/http"
 	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-/////------ schema ------////
+// K8SecretSchema returns a Terraform resource schema for an ECS Service
 func K8SecretSchema() *map[string]*schema.Schema {
 	return &map[string]*schema.Schema{
-		"secret_name": &schema.Schema{
+		"secret_name": {
 			Type:     schema.TypeString,
 			Required: true,
 			ForceNew: true,
 		},
-		"secret_type": &schema.Schema{
+		"secret_type": {
 			Type:     schema.TypeString,
 			Required: true,
 			ForceNew: true,
 		},
-		"tenant_id": &schema.Schema{
+		"tenant_id": {
 			Type:     schema.TypeString,
 			Required: true,
 			ForceNew: true,
 		},
-		"client_secret_version": &schema.Schema{
+		"client_secret_version": {
 			Type:     schema.TypeString,
 			Computed: true,
 		},
-		"secret_version": &schema.Schema{
+		"secret_version": {
 			Type:     schema.TypeString,
 			Computed: true,
 		},
-		"secret_data": &schema.Schema{
+		"secret_data": {
 			Type:      schema.TypeString,
 			Optional:  true,
 			Sensitive: true,
@@ -54,19 +55,19 @@ func (c *Client) K8SecretToState(pduploObject *map[string]interface{}, d *schema
 		///--- set
 		cObj := make(map[string]interface{})
 		var data = make(map[string]interface{})
-		var data_new = make(map[string]interface{})
+		var dataNew = make(map[string]interface{})
 		if duploObject["SecretData"] != nil {
 			data = duploObject["SecretData"].(map[string]interface{})
-			data_new = make(map[string]interface{})
+			dataNew = make(map[string]interface{})
 			for key, value := range data {
 				fmt.Println("Key:", key, "=>", "value:", value)
-				data_new[key] = ""
+				dataNew[key] = ""
 			}
 		}
-		data_new_str, _ := json.Marshal(data_new)
+		dataNewStr, _ := json.Marshal(dataNew)
 		///--- set
 		cObj["tenant_id"] = c.K8SecretGetTenantId(d)
-		cObj["secret_data"] = string(data_new_str)
+		cObj["secret_data"] = string(dataNewStr)
 		cObj["secret_type"] = duploObject["SecretType"].(string)
 		cObj["secret_name"] = duploObject["SecretName"].(string)
 		//log
@@ -80,32 +81,32 @@ func (c *Client) K8SecretToState(pduploObject *map[string]interface{}, d *schema
 func diffIgnoreForSecretMap(k, old, new string, d *schema.ResourceData) bool {
 	mapFieldName := "client_secret_version"
 	hashFieldName := "secret_data"
-	_, data_new := d.GetChange(hashFieldName)
-	hash_old := d.Get(mapFieldName).(string)
-	hash_new := hashForData(data_new.(string))
-	log.Printf("[TRACE] duplo-diffIgnoreForSecretMap ******** 1: hash_old %s hash_new   ", hash_new, hash_old)
-	return hash_old == hash_new
+	_, dataNew := d.GetChange(hashFieldName)
+	hashOld := d.Get(mapFieldName).(string)
+	hashNew := hashForData(dataNew.(string))
+	log.Printf("[TRACE] duplo-diffIgnoreForSecretMap ******** 1: hash_old %s hash_new   ", hashNew, hashOld)
+	return hashOld == hashNew
 }
 
 func (c *Client) DuploK8SecretFromState(d *schema.ResourceData, m interface{}, isUpdate bool) (string, error) {
 	url := c.K8SecretListUrl(d)
-	var api_str = fmt.Sprintf("duplo-DuploK8SecretFromState-Create %s ", url)
+	var apiStr = fmt.Sprintf("duplo-DuploK8SecretFromState-Create %s ", url)
 	if isUpdate {
-		api_str = fmt.Sprintf("duplo-DuploK8SecretFromState-update %s ", url)
+		apiStr = fmt.Sprintf("duplo-DuploK8SecretFromState-update %s ", url)
 	}
-	log.Printf("[TRACE] %s 1 ********: %s", api_str, url)
+	log.Printf("[TRACE] %s 1 ********: %s", apiStr, url)
 	//
 	duploObject := make(map[string]interface{})
 	///--- set
-	prev, new_secret_data := d.GetChange("secret_data")
-	log.Printf("[TRACE] %s 2 ********: api_str,  prev, new_secret_data %s %s ", api_str, prev, new_secret_data)
+	prev, newSecretData := d.GetChange("secret_data")
+	log.Printf("[TRACE] %s 2 ********: api_str,  prev, new_secret_data %s %s ", apiStr, prev, newSecretData)
 	duploObject["SecretData"] = make(map[string]interface{})
 	data := make(map[string]interface{})
-	data_str := d.Get("secret_data").(string)
-	log.Printf("[TRACE] %s 2 ********: data_str %s", api_str, data_str)
-	err := json.Unmarshal([]byte(data_str), &data)
+	dataStr := d.Get("secret_data").(string)
+	log.Printf("[TRACE] %s 2 ********: data_str %s", apiStr, dataStr)
+	err := json.Unmarshal([]byte(dataStr), &data)
 	if err != nil {
-		log.Printf("[TRACE] %s 3 ********: err %s", api_str, err.Error())
+		log.Printf("[TRACE] %s 3 ********: err %s", apiStr, err.Error())
 	}
 	//
 	duploObject["SecretData"] = data
@@ -113,11 +114,11 @@ func (c *Client) DuploK8SecretFromState(d *schema.ResourceData, m interface{}, i
 	duploObject["SecretVersion"] = d.Get("secret_version").(string)
 	duploObject["SecretType"] = d.Get("secret_type").(string)
 	//keep hash
-	d.Set("client_secret_version", hashForData(string(data_str)))
+	d.Set("client_secret_version", hashForData(string(dataStr)))
 
 	//log
 	jsonData, _ := json.Marshal(&duploObject)
-	log.Printf("[TRACE] %s 5 ********: to-CLOUD-all %s", api_str, jsonData)
+	log.Printf("[TRACE] %s 5 ********: to-CLOUD-all %s", apiStr, jsonData)
 	return string(jsonData), nil
 }
 
@@ -129,10 +130,10 @@ func (c *Client) DuploK8SecretSetIdFromCloud(duploObject *map[string]interface{}
 	return d.Id()
 }
 func (c *Client) K8SecretSetId(d *schema.ResourceData) string {
-	tenant_id := c.K8SecretGetTenantId(d)
+	tenantID := c.K8SecretGetTenantId(d)
 	name := d.Get("secret_name").(string)
 	///--- set
-	id := fmt.Sprintf("v2/subscriptions/%s/K8SecretApiV2/%s", tenant_id, name)
+	id := fmt.Sprintf("v2/subscriptions/%s/K8SecretApiV2/%s", tenantID, name)
 	d.SetId(id)
 	return id
 }
@@ -147,8 +148,8 @@ func (c *Client) K8SecretUrl(d *schema.ResourceData) string {
 
 // app for -- get list + create + update
 func (c *Client) K8SecretListUrl(d *schema.ResourceData) string {
-	tenant_id := c.K8SecretGetTenantId(d)
-	api := fmt.Sprintf("v2/subscriptions/%s/K8SecretApiV2", tenant_id)
+	tenantID := c.K8SecretGetTenantId(d)
+	api := fmt.Sprintf("v2/subscriptions/%s/K8SecretApiV2", tenantID)
 	host := fmt.Sprintf("%s/%s", c.HostURL, api)
 	log.Printf("[TRACE] duplo-K8SecretListUrl %s 1 ********: %s", api, host)
 	return host
@@ -156,23 +157,23 @@ func (c *Client) K8SecretListUrl(d *schema.ResourceData) string {
 
 // tenant_id or any  parents in import url should be handled if not part of get json
 func (c *Client) K8SecretGetTenantId(d *schema.ResourceData) string {
-	tenant_id := d.Get("tenant_id").(string)
+	tenantID := d.Get("tenant_id").(string)
 	//tenant_id is local only field --- should be returned from server
-	if tenant_id == "" {
+	if tenantID == "" {
 		id := d.Id()
-		id_array := strings.Split(id, "/")
-		for i, s := range id_array {
+		idArray := strings.Split(id, "/")
+		for i, s := range idArray {
 			if s == "subscriptions" {
-				next_i := i + 1
-				if id_array[next_i] != "" {
-					d.Set("tenant_id", id_array[next_i])
+				j := i + 1
+				if idArray[j] != "" {
+					d.Set("tenant_id", idArray[j])
 				}
-				return id_array[next_i]
+				return idArray[j]
 			}
 			fmt.Println(i, s)
 		}
 	}
-	return tenant_id
+	return tenantID
 }
 
 //Utils convert for get-list   -- cloud names (CamelCase) to tf names (SnakeCase)
@@ -206,15 +207,15 @@ func (c *Client) K8SecretFillGet(duploObject *map[string]interface{}, d *schema.
 		}
 		return nil
 	}
-	err_msg := fmt.Errorf("K8Secret not found")
-	return err_msg
+	errMsg := fmt.Errorf("K8Secret not found")
+	return errMsg
 }
 
 /////////  API list //////////
 func (c *Client) K8SecretGetList(d *schema.ResourceData, m interface{}) (*[]map[string]interface{}, error) {
 	//todo: filter other than tenant
 	filters, filtersOk := d.GetOk("filter")
-	log.Printf("[TRACE] K8SecretGetList filters 1 ********* : %s  %s", filters, filtersOk)
+	log.Printf("[TRACE] K8SecretGetList filters 1 ********* : %s  %v", filters, filtersOk)
 	//
 	api := c.K8SecretListUrl(d)
 	url := api
@@ -234,7 +235,7 @@ func (c *Client) K8SecretGetList(d *schema.ResourceData, m interface{}) (*[]map[
 	if err != nil {
 		return nil, err
 	}
-	log.Printf("[TRACE] duplo-K8SecretGetList 5 %s  ********: %s", api, len(duploObjects))
+	log.Printf("[TRACE] duplo-K8SecretGetList 5 %s  ********: %d", api, len(duploObjects))
 
 	return &duploObjects, nil
 }
@@ -268,8 +269,8 @@ func (c *Client) K8SecretGet(d *schema.ResourceData, m interface{}) error {
 		log.Printf("[TRACE] duplo-K8SecretGet 6 FOUND *****", api)
 		return nil
 	}
-	err_msg := fmt.Errorf("K8Secret not found  : %s body:%s", api, bodyString)
-	return err_msg
+	errMsg := fmt.Errorf("K8Secret not found  : %s body:%s", api, bodyString)
+	return errMsg
 }
 
 /////////  API Item //////////
@@ -288,42 +289,42 @@ func (c *Client) K8SecretCreateOrUpdate(d *schema.ResourceData, m interface{}, i
 	url := c.K8SecretListUrl(d)
 	api := url
 	var action = "POST"
-	var api_str = fmt.Sprintf("duplo-K8SecretCreate %s ", api)
+	var apiStr = fmt.Sprintf("duplo-K8SecretCreate %s ", api)
 	if isUpdate {
 		action = "PUT"
-		api_str = fmt.Sprintf("duplo-K8SecretUpdate %s ", api)
+		apiStr = fmt.Sprintf("duplo-K8SecretUpdate %s ", api)
 	}
-	log.Printf("[TRACE] %s 1 ********: %s", api_str, url)
+	log.Printf("[TRACE] %s 1 ********: %s", apiStr, url)
 	//
-	json_str, _ := c.DuploK8SecretFromState(d, m, isUpdate)
-	log.Printf("[TRACE] %s 4 ********: %s", api_str, json_str)
-	req, err := http.NewRequest(action, url, strings.NewReader(json_str))
+	jsonStr, _ := c.DuploK8SecretFromState(d, m, isUpdate)
+	log.Printf("[TRACE] %s 4 ********: %s", apiStr, jsonStr)
+	req, err := http.NewRequest(action, url, strings.NewReader(jsonStr))
 	if err != nil {
-		log.Printf("[TRACE] %s 5 ********: %s", api_str, err.Error())
+		log.Printf("[TRACE] %s 5 ********: %s", apiStr, err.Error())
 		return nil, err
 	}
 
 	body, err := c.doRequest(req)
 	if err != nil {
-		log.Printf("[TRACE] %s 6 ********: %s", api_str, err.Error())
+		log.Printf("[TRACE] %s 6 ********: %s", apiStr, err.Error())
 		return nil, err
 	}
 	if body != nil {
 		bodyString := string(body)
-		log.Printf("[TRACE] %s 7 ********: %s", api_str, bodyString)
+		log.Printf("[TRACE] %s 7 ********: %s", apiStr, bodyString)
 
 		duploObject := map[string]interface{}{}
 		err = json.Unmarshal(body, &duploObject)
 		if err != nil {
-			log.Printf("[TRACE] %s 8 ********:  error: %s", api_str, err.Error())
+			log.Printf("[TRACE] %s 8 ********:  error: %s", apiStr, err.Error())
 			return nil, err
 		}
-		log.Printf("[TRACE] %s 9 ******** ", api_str)
+		log.Printf("[TRACE] %s 9 ******** ", apiStr)
 		c.DuploK8SecretSetIdFromCloud(&duploObject, d)
 		return nil, nil
 	}
-	err_msg := fmt.Errorf("ERROR: in create %d,   body: %s", api, body)
-	return nil, err_msg
+	errMsg := fmt.Errorf("ERROR: in create %s,   body: %s", api, body)
+	return nil, errMsg
 }
 
 /////////  API Create/update //////////
@@ -351,7 +352,7 @@ func (c *Client) K8SecretDelete(d *schema.ResourceData, m interface{}) (*map[str
 		//nothing ?
 	}
 
-	log.Printf("[TRACE] DONE duplo-K8SecretDelete %s 4 ********: %s", api)
+	log.Printf("[TRACE] DONE duplo-K8SecretDelete %s 4 ********", api)
 	return nil, nil
 }
 
