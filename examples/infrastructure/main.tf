d50395b68b63633c816f6f26cf151d56336c545f
Joe Khoobyar <joe@duplocloud.net>
2021-03-01 16:16:22 -0500

    [release] bump version to 0.4.9

--- examples/infrastructure/main.tf
+++ examples/infrastructure/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     duplocloud = {
-      version = "0.4.8" # RELEASE VERSION
+      version = "0.4.9" # RELEASE VERSION
       source = "registry.terraform.io/duplocloud/duplocloud"
     }
   }
