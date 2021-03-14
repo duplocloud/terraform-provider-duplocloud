7a75898e4ac468c27b5b8614e812242c0c88a601
Joe Khoobyar <joe@duplocloud.net>
2021-03-14 16:40:16 -0400

    reduce code duplication

--- duplosdk/aws_elasticsearch.go
+++ duplosdk/aws_elasticsearch.go
@@ -1,10 +1,8 @@
 package duplosdk
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 	"strings"
 )
 
@@ -96,38 +94,27 @@ type DuploElasticSearchDomainRequest struct {
 
 // TenantListElasticSearchDomains retrieves a list of AWS ElasticSearch domains.
 func (c *Client) TenantListElasticSearchDomains(tenantID string) (*[]DuploElasticSearchDomain, error) {
-
-	// Format the URL
-	url := fmt.Sprintf("%s/subscriptions/%s/GetElasticSearchDomains", c.HostURL, tenantID)
-	log.Printf("[TRACE] duplo-TenantListElasticSearchDomains 1 ********: %s ", url)
+	apiName := fmt.Sprintf("TenantListElasticSearchDomains(%s)", tenantID)
+	list := []DuploElasticSearchDomain{}
 
 	// Get the list from Duplo
-	req2, _ := http.NewRequest("GET", url, nil)
-	body, err := c.doRequest(req2)
+	err := c.getAPI(apiName, fmt.Sprintf("subscriptions/%s/GetElasticSearchDomains", tenantID), &list)
 	if err != nil {
-		log.Printf("[TRACE] duplo-TenantListElasticSearchDomains 2 ********: %s", err.Error())
 		return nil, err
 	}
-	bodyString := string(body)
-	log.Printf("[TRACE] duplo-TenantListElasticSearchDomains 3 ********: %s", bodyString)
 
-	// Return it as an object.
-	duploObjects := make([]DuploElasticSearchDomain, 0)
-	err = json.Unmarshal(body, &duploObjects)
-	if err != nil {
-		return nil, err
-	}
-	log.Printf("[TRACE] duplo-TenantListElasticSearchDomains 4 ********")
-	for i := range duploObjects {
-		parts := strings.SplitN(duploObjects[i].DomainName, "-", 3)
-		name := duploObjects[i].DomainName
+	// Add the tenant ID and name to each element and return the list.
+	log.Printf("[TRACE] %s: %d items", apiName, len(list))
+	for i := range list {
+		parts := strings.SplitN(list[i].DomainName, "-", 3)
+		name := list[i].DomainName
 		if len(parts) == 3 {
 			name = parts[2]
 		}
-		duploObjects[i].TenantID = tenantID
-		duploObjects[i].Name = name
+		list[i].TenantID = tenantID
+		list[i].Name = name
 	}
-	return &duploObjects, nil
+	return &list, nil
 }
 
 // TenantGetElasticSearchDomain retrieves a single AWS ElasticSearch domains from the list returned by Duplo.
@@ -155,64 +142,20 @@ func (c *Client) TenantGetElasticSearchDomain(tenantID string, name string, dele
 
 // TenantUpdateElasticSearchDomain creates a single AWS ElasticSearch domain via Duplo.
 func (c *Client) TenantUpdateElasticSearchDomain(tenantID string, duplo *DuploElasticSearchDomainRequest) error {
-	// Build the request
-	rqBody, err := json.Marshal(&duplo)
-	if err != nil {
-		log.Printf("[TRACE] TenantUpdateElasticSearchDomain 1 JSON gen : %s", err.Error())
-		return err
-	}
-	url := fmt.Sprintf("%s/subscriptions/%s/ElasticSearchDomainUpdate", c.HostURL, tenantID)
-	log.Printf("[TRACE] TenantUpdateElasticSearchDomain 2 : %s <= %s", url, rqBody)
-	req, err := http.NewRequest("POST", url, strings.NewReader(string(rqBody)))
-	if err != nil {
-		log.Printf("[TRACE] TenantUpdateElasticSearchDomain 3 HTTP builder : %s", err.Error())
-		return err
-	}
-
-	// Call the API and get the response
-	body, err := c.doRequest(req)
-	if err != nil {
-		log.Printf("[TRACE] TenantUpdateElasticSearchDomain 4 HTTP POST : %s", err.Error())
-		return fmt.Errorf("Tenant %s failed to create ElasticSearch domain %s: '%s'", tenantID, duplo.Name, err)
-	}
-	bodyString := string(body)
-
-	// Expect the response to be "null"
-	if bodyString == "null" {
-		return nil
-	}
-	return fmt.Errorf("Tenant %s failed to create ElasticSearch domain %s: '%s'", tenantID, duplo.Name, bodyString)
+	// Create the ES domain via Duplo.
+	return c.postAPI(
+		fmt.Sprintf("TenantUpdateElasticSearchDomain(%s, %s)", tenantID, duplo.Name),
+		fmt.Sprintf("subscriptions/%s/ElasticSearchDomainUpdate", tenantID),
+		&duplo,
+		nil)
 }
 
 // TenantDeleteElasticSearchDomain deletes a single AWS ElasticSearch domain via Duplo.
 func (c *Client) TenantDeleteElasticSearchDomain(tenantID string, domainName string) error {
-	duplo := DuploElasticSearchDomainRequest{Name: domainName, State: "delete"}
-
-	// Build the request
-	rqBody, err := json.Marshal(&duplo)
-	if err != nil {
-		log.Printf("[TRACE] TenantDeleteElasticSearchDomain 1 JSON gen : %s", err.Error())
-		return err
-	}
-	url := fmt.Sprintf("%s/subscriptions/%s/ElasticSearchDomainUpdate", c.HostURL, tenantID)
-	log.Printf("[TRACE] TenantDeleteElasticSearchDomain 2 : %s <= %s", url, rqBody)
-	req, err := http.NewRequest("POST", url, strings.NewReader(string(rqBody)))
-	if err != nil {
-		log.Printf("[TRACE] TenantDeleteElasticSearchDomain 3 HTTP builder : %s", err.Error())
-		return err
-	}
-
-	// Call the API and get the response
-	body, err := c.doRequest(req)
-	if err != nil {
-		log.Printf("[TRACE] TenantDeleteElasticSearchDomain 4 HTTP POST : %s", err.Error())
-		return fmt.Errorf("Tenant %s failed to delete ElasticSearch domain %s: '%s'", tenantID, duplo.Name, err)
-	}
-	bodyString := string(body)
-
-	// Expect the response to be "null"
-	if bodyString == "null" {
-		return nil
-	}
-	return fmt.Errorf("Tenant %s failed to delete ElasticSearch domain %s: '%s'", tenantID, duplo.Name, bodyString)
+	// Delete the ES domain via Duplo.
+	return c.postAPI(
+		fmt.Sprintf("TenantDeleteElasticSearchDomain(%s, %s)", tenantID, domainName),
+		fmt.Sprintf("subscriptions/%s/ElasticSearchDomainUpdate", tenantID),
+		&DuploElasticSearchDomainRequest{Name: domainName, State: "delete"},
+		nil)
 }
