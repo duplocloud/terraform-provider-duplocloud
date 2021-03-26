f85d9b7ad9a412398bf03c75d5daa1ef8b5cf142
Joe Khoobyar <joe@duplocloud.net>
2021-03-26 10:08:26 -0400

    version bump for 0.4.x train

--- examples/k8/main.tf
+++ examples/k8/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     duplocloud = {
-      version = "0.4.9" # RELEASE VERSION
+      version = "0.4.10" # RELEASE VERSION
       source = "registry.terraform.io/duplocloud/duplocloud"
     }
   }
