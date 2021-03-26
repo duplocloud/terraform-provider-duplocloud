ac060c37b849ef20266d75c6875b4aa74762095b
Joe Khoobyar <joe@duplocloud.net>
2021-03-26 17:54:34 -0400

    [release] bump version to 0.5.13

--- examples/ecs/main.tf
+++ examples/ecs/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     duplocloud = {
-      version = "0.5.12" # RELEASE VERSION
+      version = "0.5.13" # RELEASE VERSION
       source = "registry.terraform.io/duplocloud/duplocloud"
     }
   }
