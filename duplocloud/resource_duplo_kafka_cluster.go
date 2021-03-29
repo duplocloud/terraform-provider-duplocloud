cf612d6f7975ac9f3ddec4776f133c5f6f52e93e
Joe Khoobyar <joe@duplocloud.net>
2021-03-29 17:32:28 -0400

    bump timeouts for kafka clusters

--- duplocloud/resource_duplo_kafka_cluster.go
+++ duplocloud/resource_duplo_kafka_cluster.go
@@ -101,8 +101,8 @@ func resourceAwsKafkaCluster() *schema.Resource {
 			State: schema.ImportStatePassthrough,
 		},
 		Timeouts: &schema.ResourceTimeout{
-			Create: schema.DefaultTimeout(5 * time.Minute),
-			Delete: schema.DefaultTimeout(2 * time.Minute),
+			Create: schema.DefaultTimeout(30 * time.Minute),
+			Delete: schema.DefaultTimeout(5 * time.Minute),
 		},
 		Schema: kafkaClusterSchema(),
 	}
