6e7e6eeeb8ee6842e4f18866136a424ce53e8d81
Joe Khoobyar <joe@duplocloud.net>
2021-08-06 12:26:39 -0400

    [release] bump version to 0.6.12

--- examples/eks-integration/main.tf
+++ examples/eks-integration/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     duplocloud = {
-      version = "0.6.11" # RELEASE VERSION
+      version = "0.6.12" # RELEASE VERSION
       source  = "registry.terraform.io/duplocloud/duplocloud"
     }
     aws = {
