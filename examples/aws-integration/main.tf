d7a4b2e341114c5079c19723354963ba22de313e
Joe Khoobyar <joe@duplocloud.net>
2021-08-11 17:03:10 -0400

    [release] bump version to 0.6.15

--- examples/aws-integration/main.tf
+++ examples/aws-integration/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     duplocloud = {
-      version = "0.6.14" # RELEASE VERSION
+      version = "0.6.15" # RELEASE VERSION
       source  = "registry.terraform.io/duplocloud/duplocloud"
     }
     aws = {
