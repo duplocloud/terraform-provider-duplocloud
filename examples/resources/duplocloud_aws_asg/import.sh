da3a697ac1e0b5f7a67017c626e8174e4980c427
tahir-duplo <tahir@duplocloud.net>
2021-09-11 20:00:27 +0530

    Minor change in service name.

--- examples/resources/duplocloud_aws_asg/import.sh
+++ examples/resources/duplocloud_aws_asg/import.sh
@@ -1,5 +1,5 @@
 # Example: Importing an existing AWS ASG
 #  - *TENANT_ID* is the tenant GUID
-#  - *FRIENDLY_NAME* is the duploservice-<account_name>-<friendly_name>
+#  - *FRIENDLY_NAME* is the duploservices-<account_name>-<friendly_name>
 #
 terraform import duplocloud_asg_profile.myAsgProfile *TENANT_ID*/*FRIENDLY_NAME*
