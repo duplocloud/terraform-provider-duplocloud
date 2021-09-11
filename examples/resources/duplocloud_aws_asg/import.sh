fa1c6cbcdf8c25a4c2e588da2a5d853b86dd0fe6
tahir-duplo <tahir@duplocloud.net>
2021-09-11 16:12:29 +0530

    Documentation and example added.

---
+++ examples/resources/duplocloud_aws_asg/import.sh
@@ -0,0 +1,5 @@
+# Example: Importing an existing AWS ASG
+#  - *TENANT_ID* is the tenant GUID
+#  - *FRIENDLY_NAME* is the duploservice-<account_name>-<friendly_name>
+#
+terraform import duplocloud_aws_host.myhost *TENANT_ID*/*FRIENDLY_NAME*
