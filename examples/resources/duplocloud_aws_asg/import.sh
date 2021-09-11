4df365f9c56c9de95c5e3ccea0fdfc44cbb9d2fa
tahir-duplo <tahir@duplocloud.net>
2021-09-11 17:30:22 +0530

    import file fied.

--- examples/resources/duplocloud_aws_asg/import.sh
+++ examples/resources/duplocloud_aws_asg/import.sh
@@ -2,4 +2,4 @@
 #  - *TENANT_ID* is the tenant GUID
 #  - *FRIENDLY_NAME* is the duploservice-<account_name>-<friendly_name>
 #
-terraform import duplocloud_aws_host.myhost *TENANT_ID*/*FRIENDLY_NAME*
+terraform import duplocloud_asg_profile.myAsgProfile *TENANT_ID*/*FRIENDLY_NAME*
