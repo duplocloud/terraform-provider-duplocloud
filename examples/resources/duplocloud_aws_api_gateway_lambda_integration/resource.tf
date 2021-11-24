0b6f58e04e4ab569dd2f14590f6ac4e55af2fe64
tahir-duplo <tahir@duplocloud.net>
2021-11-24 23:16:44 +0530

    duplocloud_aws_api_gateway_integration added to manage a aws api gateway in Duplo.

---
+++ examples/resources/duplocloud_aws_api_gateway_lambda_integration/resource.tf
@@ -0,0 +1,10 @@
+resource "duplocloud_tenant" "duplo-app" {
+  account_name = "duplo-app"
+  plan_id      = "default"
+}
+
+resource "duplocloud_aws_api_gateway_integration" "apigw-lambda" {
+  tenant_id            = duplocloud_tenant.duplo-app.tenant_id
+  name                 = "test-api-lambda"
+  lambda_function_name = "duploservices-default-test-lambda-123456789"
+}
