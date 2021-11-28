24d1366fdb8908df300f39c314d2d68e6692e780
Joe Khoobyar <joe@duplocloud.net>
2021-11-27 20:52:39 -0500

    [release] bump version to 0.6.20

--- examples/emr/files/emr_spot/main.tf
+++ examples/emr/files/emr_spot/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     duplocloud = {
-      version = "0.6.19" # RELEASE VERSION
+      version = "0.6.20" # RELEASE VERSION
       source  = "registry.terraform.io/duplocloud/duplocloud"
     }
   }
