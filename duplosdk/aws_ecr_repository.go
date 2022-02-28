040c23adc8f2daebcad260877a4e69d64ce21525
Tahir Tamboli <89899853+tahir-duplo@users.noreply.github.com>
2022-03-01 01:42:36 +0530

    ECR repository and other agent support is added. (#48)
    * ECR repository support is added.
    
    * Other agent support is added.
    
    * lint: go fmt
    
    * Node list added in state.
    
    Co-authored-by: Duplo Githug Bot <joe+github-bot@duplocloud.net>

---
+++ duplosdk/aws_ecr_repository.go
@@ -0,0 +1,78 @@
+package duplosdk
+
+import (
+	"fmt"
+)
+
+type DuploAwsEcrRepositoryRequest struct {
+	KmsEncryption         string `json:"KmsEncryption,omitempty"`
+	EnableTagImmutability bool   `json:"EnableTagImmutability,omitempty"`
+	EnableScanImageOnPush bool   `json:"EnableScanImageOnPush,omitempty"`
+	Name                  string `json:"Name"`
+}
+
+type DuploAwsEcrRepository struct {
+	KmsEncryption         string `json:"KmsEncryption,omitempty"`
+	KmsEncryptionAlias    string `json:"KmsEncryptionAlias,omitempty"`
+	EnableTagImmutability bool   `json:"EnableTagImmutability,omitempty"`
+	EnableScanImageOnPush bool   `json:"EnableScanImageOnPush,omitempty"`
+	Arn                   string `json:"Arn"`
+	ResourceType          int    `json:"ResourceType,omitempty"`
+	Name                  string `json:"Name"`
+	RegistryId            string `json:"RegistryId,omitempty"`
+	RepositoryUri         string `json:"RepositoryUri,omitempty"`
+}
+
+func (c *Client) AwsEcrRepositoryCreate(tenantID string, rq *DuploAwsEcrRepositoryRequest) ClientError {
+	rp := DuploAwsEcrRepository{}
+	return c.postAPI(
+		fmt.Sprintf("AwsEcrRepositoryCreate(%s, %s)", tenantID, rq.Name),
+		fmt.Sprintf("v3/subscriptions/%s/aws/ecrRepository", tenantID),
+		&rq,
+		&rp,
+	)
+}
+
+func (c *Client) AwsEcrRepositoryGet(tenantID string, name string) (*DuploAwsEcrRepository, ClientError) {
+	rp := DuploAwsEcrRepository{}
+	err := c.getAPI(
+		fmt.Sprintf("AwsEcrRepositoryGet(%s)", tenantID),
+		fmt.Sprintf("v3/subscriptions/%s/aws/ecrRepository/%s", tenantID, name),
+		&rp,
+	)
+	return &rp, err
+}
+
+func (c *Client) AwsEcrRepositoryList(tenantID string) (*[]DuploAwsEcrRepository, ClientError) {
+	rp := []DuploAwsEcrRepository{}
+	err := c.getAPI(
+		fmt.Sprintf("AwsEcrRepositoryList(%s)", tenantID),
+		fmt.Sprintf("v3/subscriptions/%s/aws/ecrRepository", tenantID),
+		&rp,
+	)
+	return &rp, err
+}
+
+func (c *Client) AwsEcrRepositoryExists(tenantID, name string) (bool, ClientError) {
+	list, err := c.AwsEcrRepositoryList(tenantID)
+	if err != nil {
+		return false, err
+	}
+
+	if list != nil {
+		for _, element := range *list {
+			if element.Name == name {
+				return true, nil
+			}
+		}
+	}
+	return false, nil
+}
+
+func (c *Client) AwsEcrRepositoryDelete(tenantID string, name string) ClientError {
+	return c.deleteAPI(
+		fmt.Sprintf("AwsEcrRepositoryDelete(%s, %s)", tenantID, name),
+		fmt.Sprintf("v3/subscriptions/%s/aws/ecrRepository/%s", tenantID, name),
+		nil,
+	)
+}
---
+++ examples/resources/duplocloud_aws_ecr_repository/import.sh
@@ -0,0 +1,5 @@
+# Example: Importing an existing AWS ECR repository
+#  - *TENANT_ID* is the tenant GUID
+#  - *SHORT_NAME* is the short name of the AWS ECR repository
+#
+terraform import duplocloud_aws_ecr_repository.myecr *TENANT_ID*/*SHORT_NAME*
