267a7eeb98d131b21918e8dbb98c774a9342ff29
Joe Khoobyar <joe@duplocloud.net>
2022-03-22 13:45:04 -0400

    use V1 API for create/update service params

--- duplosdk/duplo_service_params.go
+++ duplosdk/duplo_service_params.go
@@ -32,40 +32,6 @@ func (c *Client) DuploServiceParamsGetList(tenantID string) (*[]DuploServicePara
 	return &list, nil
 }
 
-// DuploServiceParamsCreate creates a service's load balancer via the Duplo API.
-func (c *Client) DuploServiceParamsCreate(tenantID string, rq *DuploServiceParams) (*DuploServiceParams, ClientError) {
-	return c.DuploServiceParamsCreateOrUpdate(tenantID, rq, false)
-}
-
-// DuploServiceParamsUpdate updates an service's load balancer via the Duplo API.
-func (c *Client) DuploServiceParamsUpdate(tenantID string, rq *DuploServiceParams) (*DuploServiceParams, ClientError) {
-	return c.DuploServiceParamsCreateOrUpdate(tenantID, rq, true)
-}
-
-// DuploServiceParamsCreateOrUpdate creates or updates an service's load balancer via the Duplo API.
-func (c *Client) DuploServiceParamsCreateOrUpdate(tenantID string, rq *DuploServiceParams, updating bool) (*DuploServiceParams, ClientError) {
-
-	// Build the request
-	verb := "POST"
-	if updating {
-		verb = "PUT"
-	}
-
-	// Call the API.
-	rp := DuploServiceParams{}
-	err := c.doAPIWithRequestBody(
-		verb,
-		fmt.Sprintf("DuploServiceParamsCreateOrUpdate(%s, %s)", tenantID, rq.ReplicationControllerName),
-		fmt.Sprintf("v2/subscriptions/%s/ReplicationControllerParamsV2", tenantID),
-		&rq,
-		&rp,
-	)
-	if err != nil {
-		return nil, err
-	}
-	return &rp, err
-}
-
 // DuploServiceParamsDelete deletes a service's load balancer via the Duplo API.
 func (c *Client) DuploServiceParamsDelete(tenantID string, name string) ClientError {
 	// Delete the service parameters
