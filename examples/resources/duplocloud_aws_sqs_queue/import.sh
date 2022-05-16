7ebd053e00b7582c93a5337c0b9698021fbaca35
tahir-duplo <tahir@duplocloud.net>
2022-05-16 22:01:24 +0530

    SQS support added.

---
+++ examples/resources/duplocloud_aws_sqs_queue/import.sh
@@ -0,0 +1,5 @@
+# Example: Importing an existing AWS SQS Queue
+#  - *TENANT_ID* is the tenant GUID
+#  - *URL* The URL for the created Amazon SQS queue.
+#
+terraform import duplocloud_aws_sqs_queue.sqs_queue *TENANT_ID*/*URL*
