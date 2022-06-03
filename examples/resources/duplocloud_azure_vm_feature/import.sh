dfa7dd2c4b470d2085c18acf713d2b3db908043c
tahir-duplo <tahir@duplocloud.net>
2022-06-03 21:27:46 +0530

    Resource duplocloud_azure_vm_feature added.

---
+++ examples/resources/duplocloud_azure_vm_feature/import.sh
@@ -0,0 +1,6 @@
+# Example: Importing an existing Azure Virtual Machine Feature
+#  - *TENANT_ID* is the tenant GUID
+#  - *VM_NAME* is the short name of the Azure Virtual Machine
+#  - *FEATURE_NAME* is the short name of the Azure Virtual Machine Feature
+#
+terraform import duplocloud_azure_vm_feature.vm_feature *TENANT_ID*/*VM_NAME*/*FEATURE_NAME*
