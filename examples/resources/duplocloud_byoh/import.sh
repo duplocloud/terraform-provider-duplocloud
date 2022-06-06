a33e6075172773b2f7eec14bfdec4cebd3459cdd
tahir-duplo <tahir@duplocloud.net>
2022-06-07 01:25:49 +0530

    Resource duplocloud_byoh added.

---
+++ examples/resources/duplocloud_byoh/import.sh
@@ -0,0 +1,5 @@
+# Example: Importing an existing BYOH Instance.
+#  - *TENANT_ID* is the tenant GUID
+#  - *NAME* is the name of BYOH Instance.
+#
+terraform import duplocloud_byoh.byoh *TENANT_ID*/*NAME*
