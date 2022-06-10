5293a8a04389f81484596b2761437f335a290777
tahir-duplo <tahir@duplocloud.net>
2022-06-10 21:44:45 +0530

    duplocloud_azure_vault_backup_policy resource added.

--- duplocloud/provider.go
+++ duplocloud/provider.go
@@ -117,6 +117,7 @@ func Provider() *schema.Provider {
 			"duplocloud_azure_log_analytics_workspace":   resourceAzureLogAnalyticsWorkspace(),
 			"duplocloud_azure_recovery_services_vault":   resourceAzureRecoveryServicesVault(),
 			"duplocloud_azure_vm_feature":                resourceAzureVmFeature(),
+			"duplocloud_azure_vault_backup_policy":       resourceAzureVaultBackupPolicy(),
 			"duplocloud_other_agents":                    resourceOtherAgents(),
 			"duplocloud_byoh":                            resourceByoh(),
 		},
