f25b6b7bf27025180758b672609446cf36ff97bb
Pravin Reddy <pravin@duplocloud.net>
2023-05-05 09:51:57 -0700

    or changes

--- examples/resources/duplocloud_rds_instance/import.sh
+++ examples/resources/duplocloud_rds_instance/import.sh
@@ -2,4 +2,4 @@
 #  - *TENANT_ID* is the tenant GUID
 #  - *SHORTNAME* is the short name of the database (without the duplo prefix)
 #
-terraform import duplocloud_rds_instance.mydb v3/subscriptions/*TENANT_ID*/RDSDBInstance/*SHORTNAME*
+terraform import duplocloud_rds_instance.mydb v2/subscriptions/*TENANT_ID*/RDSDBInstance/*SHORTNAME*
