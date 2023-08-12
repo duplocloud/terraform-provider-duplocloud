37944a341f3fb49ff63c4449ccd9b03bf03553c1
tahir-duplo <tahir@duplocloud.net>
2023-08-12 14:08:56 +0530

    minor changes.

--- duplosdk/aws_cloudwatch.go
+++ duplosdk/aws_cloudwatch.go
@@ -5,7 +5,7 @@ import "fmt"
 type DuploCloudWatchEventRule struct {
 	Name               string                 `json:"Name"`
 	Description        string                 `json:"Description,omitempty"`
-	ScheduleExpression string                 `json:"ScheduleExpression"`
+	ScheduleExpression string                 `json:"ScheduleExpression,omitempty"`
 	State              string                 `json:"State"`
 	Tags               *[]DuploKeyStringValue `json:"Tags,omitempty"`
 	EventBusName       string                 `json:"EventBusName,omitempty"`
