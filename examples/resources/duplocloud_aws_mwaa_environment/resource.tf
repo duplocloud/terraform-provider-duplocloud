3caa93825f0db64fcb6b4d49fdbb3cbf91a6d908
Pravin Reddy <pravin@duplocloud.net>
2023-09-08 10:57:43 -0700

    pr changes

--- examples/resources/duplocloud_aws_mwaa_environment/resource.tf
+++ examples/resources/duplocloud_aws_mwaa_environment/resource.tf
@@ -8,15 +8,24 @@ data "duplocloud_tenant_aws_kms_key" "tenant_kms_key" {
 }
 
 resource "duplocloud_aws_mwaa_environment" "my-mwaa" {
-  tenant_id                       = duplocloud_tenant.myapp.tenant_id
-  name                            = "airflow-test"
-  source_bucket_arn               = "arn:aws:s3:::duploservices-demo01-dags-140563923322"
-  dag_s3_path                     = "AirflowDags/dag"
+  tenant_id              = duplocloud_tenant.myapp.tenant_id
+  name                   = "airflow-test"
+  source_bucket_arn      = "arn:aws:s3:::xxx-xxx-xx-xxxx"
+  dag_s3_path            = "AirflowDags/dag"
+  plugins_s3_path        = "AirflowDags/plugins.zip"
+  requirements_s3_path   = "AirflowDags/requirements.txt"
+  startup_script_s3_path = "AirflowDags/startup-script.sh"
+
+  ## optional: Provide particular s3 version of the file. If empty, latest version will be used (recommended).
+  # plugins_s3_object_version = "S3 object version"
+  # requirements_s3_object_version = "S3 object version"
+  # startup_script_s3_object_version = "S3 object version"
+
   kms_key                         = data.duplocloud_tenant_aws_kms_key.tenant_kms_key.key_arn
   schedulers                      = 2
   max_workers                     = 10
   min_workers                     = 1
-  airflow_version                 = "2.2.2"
+  airflow_version                 = "2.6.3"
   weekly_maintenance_window_start = "SUN:23:30"
   environment_class               = "mw1.small"
 
