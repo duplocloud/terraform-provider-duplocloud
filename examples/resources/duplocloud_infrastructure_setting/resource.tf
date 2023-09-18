1ec9b629d11dc99fa7d3e5dd0ac19a24afc0c908
--unset <--unset>
2023-09-18 12:23:50 -0500

    replace invalid fields

--- examples/resources/duplocloud_infrastructure_setting/resource.tf
+++ examples/resources/duplocloud_infrastructure_setting/resource.tf
@@ -1,5 +1,15 @@
 resource "duplocloud_infrastructure_setting" "settings" {
   infra_name            = duplocloud_infrastructure.myinfra
-  EnableSecretCsiDriver = "true"
-  EnableAWSEfsVolumes   = "true"
+  setting = [
+    {
+      key   = "exampleKey1"
+      value = "exampleValue1"
+    },
+    {
+      key   = "exampleKey2"
+      value = "exampleValue2"
+    }
+  ]
+
+  delete_unspecified_settings = false
 }
