9f132df17a917dde23c2fb0f2ab57793fd5537cb
Christopher Bseirani <138274783+cbseirani-duplo@users.noreply.github.com>
2023-11-30 14:34:08 -0500

    update value

--- duplocloud/resource_duplo_aws_ssm_parameter.go
+++ duplocloud/resource_duplo_aws_ssm_parameter.go
@@ -43,6 +43,7 @@ func awsSsmParameterSchema() map[string]*schema.Schema {
 			Description: "The value of the SSM parameter.",
 			Type:        schema.TypeString,
 			Required:    true,
+			Optional:    false,
 			Computed:    true,
 		},
 		"description": {
