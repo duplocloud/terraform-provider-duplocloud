96db3796fec3b521cb3288eb9dd010a2ef9f5625
nikhil <nikhil.nambiar@duplocloud.net>
2024-02-09 17:30:11 +0530

    fixed getting plugin crash error when warm enabled and cold storage block removed from tf file

--- duplocloud/resource_duplo_aws_elasticsearch.go
+++ duplocloud/resource_duplo_aws_elasticsearch.go
@@ -651,7 +651,12 @@ func awsElasticSearchDomainClusterConfigFromState(m map[string]interface{}, dupl
 		duplo.InstanceType.Value = "t2.small.elasticsearch"
 	}
 	if v, ok := m["cold_storage_options"]; ok {
-		duplo.ColdStorageOptions.Enabled = v.([]interface{})[0].(map[string]interface{})["enabled"].(bool)
+		duplo.ColdStorageOptions = nil
+		obj := v.([]interface{})
+		if len(obj) > 0 {
+			duplo.ColdStorageOptions.Enabled = obj[0].(map[string]interface{})["enabled"].(bool)
+
+		}
 	}
 	if v, ok := m["warm_count"]; ok {
 		duplo.WarmCount = v.(int)
@@ -660,7 +665,11 @@ func awsElasticSearchDomainClusterConfigFromState(m map[string]interface{}, dupl
 		duplo.WarmEnabled = v.(bool)
 	}
 	if v, ok := m["warm_type"]; ok {
-		duplo.WarmType.Value = v.(string)
+		obj := v.(string)
+		duplo.WarmType = nil
+		if obj != "" {
+			duplo.WarmType.Value = obj
+		}
 	}
 
 	if v, ok := m["dedicated_master_enabled"]; ok {
