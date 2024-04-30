66e9ff5de6d9bc7903c46373adf58836ed863699
Pravin Reddy <pravin@duplocloud.net>
2024-04-29 23:40:26 -0700

    pr changes

--- examples/resources/duplocloud_admin_system_setting/import.sh
+++ examples/resources/duplocloud_admin_system_setting/import.sh
@@ -1,5 +1,5 @@
 # Example: Importing an existing duplo admin setting
-#  - *KEY_TYPE* is the type of setting key. Replace any occurrences of '/' with 'SLASH' if they exist within the KEY_TYPE.
+#  - *KEY_TYPE* is the type of setting key. Replace any occurrences of '/' with '_SLASH_' if they exist within the KEY_TYPE.
 #  - *KEY* is the key name
 #
 terraform import duplocloud_admin_system_setting.mySetting *KEY_TYPE*/*KEY*
