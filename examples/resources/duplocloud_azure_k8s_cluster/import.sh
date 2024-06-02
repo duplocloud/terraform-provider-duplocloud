809639217a866731d3c3d83fd25e85684715af95
Tahir Tamboli <tahirtamboli@Tahirs-MacBook-Pro.local>
2024-06-03 02:40:44 +0530

    New resource duplocloud_azure_k8s_cluster and duplocloud_azure_private_endpoint added.

---
+++ examples/resources/duplocloud_azure_k8s_cluster/import.sh
@@ -0,0 +1,4 @@
+# Example: Importing an existing Azure K8S Cluster
+#  - *INFRA_NAME* is the infrastructure name
+#
+terraform import duplocloud_azure_k8s_cluster.cluster v2/admin/InfrastructureV2/*INFRA_NAME*
---
+++ examples/resources/duplocloud_azure_private_endpoint/import.sh
@@ -0,0 +1,5 @@
+# Example: Importing an existing Azure Private Endpoint
+#  - *TENANT_ID* is the tenant GUID
+#  - *NAME* is the name of the Azure  Private Endpoint
+#
+terraform import duplocloud_azure_postgresql_database.myPostgreSQLDatabase *TENANT_ID*/*NAME*
