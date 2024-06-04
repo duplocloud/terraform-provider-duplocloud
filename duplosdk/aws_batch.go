d155f0f8c31ec7cb0a39408c9903bc0f7884f91b
nikhil <nikhil.nambiar@duplocloud.net>
2024-06-04 22:13:43 +0530

    removed omitempty tag from minvcpu

--- duplosdk/aws_batch.go
+++ duplosdk/aws_batch.go
@@ -98,7 +98,7 @@ type DuploAwsBatchComputeResource struct {
 	Type               *DuploStringValue                         `json:"Type,omitempty"`
 	AllocationStrategy *DuploStringValue                         `json:"AllocationStrategy,omitempty"`
 	MaxvCpus           int                                       `json:"MaxvCpus,omitempty"`
-	MinvCpus           int                                       `json:"MinvCpus,omitempty"`
+	MinvCpus           int                                       `json:"MinvCpus"`
 	DesiredvCpus       int                                       `json:"DesiredvCpus,omitempty"`
 	BidPercentage      int                                       `json:"BidPercentage,omitempty"`
 	InstanceTypes      []string                                  `json:"InstanceTypes,omitempty"`
