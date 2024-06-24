7852b19e8e603fe6641fc6a946355006550f46ff
Tahir Tamboli <tahirtamboli@Tahirs-MacBook-Pro.local>
2024-06-24 18:39:14 +0530

    Commented code removed.

--- examples/resources/duplocloud_aws_apigateway_event/resource.tf
+++ examples/resources/duplocloud_aws_apigateway_event/resource.tf
@@ -4,13 +4,11 @@ resource "duplocloud_tenant" "myapp" {
 }
 
 resource "duplocloud_aws_apigateway_event" "apigateway_event" {
-  tenant_id      = duplocloud_tenant.myapp.tenant_id
-  api_gateway_id = "t84tb3skz0"
-  method         = "POST"
-  path           = "/v2/docs"
-  cors           = true
-  #   authorization_type = "CUSTOM"
-  #   authorizer_id      = "twiws3"
+  tenant_id          = duplocloud_tenant.myapp.tenant_id
+  api_gateway_id     = "t84tb3skz0"
+  method             = "POST"
+  path               = "/v2/docs"
+  cors               = true
   authorization_type = "COGNITO_USER_POOLS"
   authorizer_id      = "gto03x"
 
