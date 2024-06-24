3a764658db55d86aa90831141c45afe1395005a6
Tahir Tamboli <89899853+tahir-duplo@users.noreply.github.com>
2024-06-24 20:51:52 +0530

    New resource `duplocloud_aws_apigateway_event` added. (#567)

---
+++ examples/resources/duplocloud_aws_apigateway_event/resource.tf
@@ -0,0 +1,20 @@
+resource "duplocloud_tenant" "myapp" {
+  account_name = "myapp"
+  plan_id      = "default"
+}
+
+resource "duplocloud_aws_apigateway_event" "apigateway_event" {
+  tenant_id          = duplocloud_tenant.myapp.tenant_id
+  api_gateway_id     = "t84tb3skz0"
+  method             = "POST"
+  path               = "/v2/docs"
+  cors               = true
+  authorization_type = "COGNITO_USER_POOLS"
+  authorizer_id      = "gto03x"
+
+  integration {
+    type    = "AWS_PROXY"
+    uri     = "arn:aws:apigateway:us-west-2:lambda:path/2015-03-31/functions/arn:aws:lambda:us-east-1:1234567890:function:duploservices-dev-valuation-test/invocations"
+    timeout = 29000
+  }
+}
