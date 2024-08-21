d268f7d922aceca0b1c5f32fb934adc93bb18386
nikhil <nikhil.nambiar@duplocloud.net>
2024-08-21 11:16:18 +0530

    updated example for duplocloud_azure_k8_node_pool resource

--- examples/resources/duplocloud_azure_k8_node_pool/resource.tf
+++ examples/resources/duplocloud_azure_k8_node_pool/resource.tf
@@ -4,12 +4,16 @@ resource "duplocloud_tenant" "myapp" {
 }
 
 resource "duplocloud_azure_k8_node_pool" "node_pool" {
-  tenant_id        = duplocloud_tenant.myapp.tenant_id
+  tenant_id        = "d60ea93e-3924-4507-8656-73571d99ae42"
   identifier       = 2
   min_capacity     = 1
   max_capacity     = 1
   desired_capacity = 1
-  vm_size          = "Standard_E16_v5"
-  wait_until_ready = false
+  vm_size          = "Standard_D2s_v3"
+  wait_until_ready = true
   allocation_tag   = "aks-test"
+  scale_priority {
+    eviction_policy = "Delete"
+    priority        = "Spot"
+  }
 }
