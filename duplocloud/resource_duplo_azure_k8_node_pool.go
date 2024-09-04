033d7e2079efff29fdd0042f3e679f34f46bae69
nikhil <nikhil.nambiar@duplocloud.net>
2024-09-04 13:27:40 +0530

    changed spot_max_price data type to float, Update custom diff function for spot_max_price, updated doc

--- duplocloud/resource_duplo_azure_k8_node_pool.go
+++ duplocloud/resource_duplo_azure_k8_node_pool.go
@@ -98,10 +98,11 @@ func duploAgentK8NodePoolSchema() map[string]*schema.Schema {
 						}, false),
 					},
 					"spot_max_price": {
-						Description: " for spot VMs sets the maximum price you're willing to pay, controlling costs, while priority.spot determines the scaling order of spot VM pools.",
-						Optional:    true,
-						Computed:    true,
-						Type:        schema.TypeString,
+						Description:  " for spot VMs sets the maximum price you're willing to pay, controlling costs, while priority.spot determines the scaling order of spot VM pools.",
+						Optional:     true,
+						Computed:     true,
+						Type:         schema.TypeFloat,
+						ValidateFunc: validation.FloatAtLeast(0.00001),
 					},
 				},
 			},
@@ -262,13 +263,9 @@ func expandAgentK8NodePool(d *schema.ResourceData) (*duplosdk.DuploAzureK8NodePo
 				data := mp.(map[string]interface{})
 				nodePool.ScaleSetPriority = data["priority"].(string)
 				nodePool.ScaleSetEvictionPolicy = data["eviction_policy"].(string)
-				spotPrice := data["spot_max_price"].(string)
-				if spotPrice != "" {
-					price, err := strconv.ParseFloat(spotPrice, 32)
-					if err != nil {
-						return nil, err
-					}
-					nodePool.SpotMaxPrice = float32(price)
+				spotPrice := data["spot_max_price"].(float64)
+				if spotPrice > 0 {
+					nodePool.SpotMaxPrice = float32(spotPrice)
 				}
 			}
 		}
@@ -345,7 +342,7 @@ func azureK8NodePoolWaitUntilReady(ctx context.Context, c *duplosdk.Client, tena
 func validateScalePriorityAttribute(ctx context.Context, diff *schema.ResourceDiff, m interface{}) error {
 	scalePriority := diff.Get("scale_priority").([]interface{})
 	mp := scalePriority[0].(map[string]interface{})
-	if mp["priority"].(string) == "Regular" && mp["spot_max_price"].(string) != "" {
+	if mp["priority"].(string) == "Regular" && mp["spot_max_price"].(float64) != 0 {
 		return errors.New("Scale Priority of type Regular does not support Spot max price")
 	}
 	if mp["priority"].(string) == "Regular" && mp["eviction_policy"].(string) != "" {
