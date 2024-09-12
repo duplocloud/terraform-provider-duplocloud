0fe0647b7547b088a1e2f05a97bbf053e6647256
nikhil <nikhil.nambiar@duplocloud.net>
2024-09-12 11:11:04 +0530

    updated documentation with performance insights example

--- examples/resources/duplocloud_rds_read_replica/resource.tf
+++ examples/resources/duplocloud_rds_read_replica/resource.tf
@@ -27,7 +27,7 @@ resource "duplocloud_rds_read_replica" "replica" {
 resource "duplocloud_rds_instance" "rds" {
   tenant_id      = duplocloud_tenant.myapp.tenant_id
   name           = "docDb"
-  engine         = 13 
+  engine         = 13
   engine_version = "3.6.0"
   size           = "db.t3.medium"
 
@@ -37,7 +37,7 @@ resource "duplocloud_rds_instance" "rds" {
   encrypt_storage                 = true
   store_details_in_secret_manager = true
   enhanced_monitoring             = 0
- 
+
 }
 
 resource "duplocloud_rds_read_replica" "replica" {
@@ -46,7 +46,7 @@ resource "duplocloud_rds_read_replica" "replica" {
   size               = "db.t3.medium"
   cluster_identifier = duplocloud_rds_instance.rds.cluster_identifier
   performance_insights {
-    enabled           = true
+    enabled          = true
     retention_period = 7
   }
 }
@@ -56,16 +56,16 @@ resource "duplocloud_rds_read_replica" "replica" {
 resource "duplocloud_rds_instance" "rds" {
   tenant_id      = duplocloud_tenant.myapp.tenant_id
   name           = "clust"
-  engine         = 8 
+  engine         = 8
   engine_version = "8.0.mysql_aurora.3.07.1"
   size           = "db.r5.large"
 
-  master_username = "myuser"
-  master_password = "Qaazwedd#1"
+  master_username                 = "myuser"
+  master_password                 = "Qaazwedd#1"
   encrypt_storage                 = true
   store_details_in_secret_manager = true
   enhanced_monitoring             = 0
- 
+
 }
 
 resource "duplocloud_rds_read_replica" "replica" {
@@ -74,7 +74,39 @@ resource "duplocloud_rds_read_replica" "replica" {
   size               = "db.r5.large"
   cluster_identifier = duplocloud_rds_instance.rds.cluster_identifier
   performance_insights {
-    enabled           = true
+    enabled          = true
+    retention_period = 7
+  }
+}
+
+//Performance insight example for instance db read replica
+resource "duplocloud_rds_instance" "mydb" {
+  tenant_id      = duplocloud_tenant.myapp.tenant_id
+  name           = "mydbpsql"
+  engine         = 1 // PostgreSQL
+  engine_version = "14.11"
+  size           = "db.t3.medium"
+
+  master_username = "myuser"
+  master_password = "Qaazwedd#1"
+
+  encrypt_storage                 = false
+  store_details_in_secret_manager = false
+  enhanced_monitoring             = 0
+  storage_type                    = "gp2"
+  performance_insights {
+    enabled          = true
+    retention_period = 7
+  }
+}
+
+resource "duplocloud_rds_read_replica" "replica" {
+  tenant_id          = duplocloud_rds_instance.mydb.tenant_id
+  name               = "inst-replica"
+  size               = "db.t3.medium"
+  cluster_identifier = duplocloud_rds_instance.mydb.cluster_identifier
+  performance_insights {
+    enabled          = true
     retention_period = 7
   }
 }
