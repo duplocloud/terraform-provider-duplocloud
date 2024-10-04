b2d2ba5772881da6fc28a6ebd4064251ecdf1763
nikhil <nikhil.nambiar@duplocloud.net>
2024-10-04 22:28:38 +0530

    removed share file traces from import files

--- examples/resources/duplocloud_azure_storage_queue/import.sh
+++ examples/resources/duplocloud_azure_storage_queue/import.sh
@@ -1,5 +1,5 @@
-# Example: Importing an existing Azure storage account share file
+# Example: Importing an existing Azure storage class queue
 #  - *TENANT_ID* is the tenant GUID
-#  - *STORAGE_ACCOUNT_NAME* is the name of the Azure storage account
+#  - *STORAGE_ACCOUNT_NAME* is the name of the Azure storage class
 #  - *NAME* is the name of the queue
 terraform import duplocloud_azure_storageclass_queue.qu *TENANT_ID*/*STORAGE_ACCOUNT_NAME*/queue/*NAME*
--- examples/resources/duplocloud_azure_storage_table/import.sh
+++ examples/resources/duplocloud_azure_storage_table/import.sh
@@ -1,5 +1,5 @@
-# Example: Importing an existing Azure storage account share file
+# Example: Importing an existing Azure storage class table
 #  - *TENANT_ID* is the tenant GUID
-#  - *STORAGE_ACCOUNT_NAME* is the name of the Azure storage account
+#  - *STORAGE_ACCOUNT_NAME* is the name of the Azure storage class
 #  - *NAME* is the name of the table
 terraform import duplocloud_azure_storageclass_table.tbl *TENANT_ID*/*STORAGE_ACCOUNT_NAME*/table/*NAME*
