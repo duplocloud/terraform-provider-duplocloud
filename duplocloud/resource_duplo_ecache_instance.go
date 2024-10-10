duplocloud/terraform-provider-duplocloud

commit ed1f996e110fc512a32a7a618acd41fbf01fba09

merging duplosdk/version_utils into duplocloud/version_utils , removed duplosdk/version_utils files, updated call heirarchy code for function called from duplosdk/version_utils

duplocloud/resource_duplo_ecache_instance.go
@@ -349,11 +349,11 @@ func validateLogDeliveryConfigurations(engineVersion string, configs []duplosdk.
 	for _, config := range configs {
 		switch config.LogType {
 		case "engine-log":
-			if !duplosdk.IsAppVersionEqualOrGreater(engineVersion, "6.2.0") {
+			if IsAppVersionEqualOrGreater(engineVersion, "6.2.0") {
 				return diag.Errorf("log_delivery_configuration with log_type 'engine-log' cannot be used with engine_version '%s'. Please use engine_version '6.2.0' or above.", engineVersion)
 			}
 		case "slow-log":
-			if !duplosdk.IsAppVersionEqualOrGreater(engineVersion, "6.0.0") {
+			if IsAppVersionEqualOrGreater(engineVersion, "6.0.0") {
 				return diag.Errorf("log_delivery_configuration with log_type 'slow-log' cannot be used with engine_version '%s'. Please use engine_version '6.0.0' or above.", engineVersion)
 			}
 		default:

duplocloud/version_utils.go
@@ -50,3 +50,44 @@ func getVersionPart(versionParts []string, index int) int {
 	}
 	return 0 // Return 0 if the index is out of bounds (e.g., comparing "1.2" with "1.2.0")
 }
+
+type AppVersion struct {
+	Major int
+	Minor int
+}
+
+func ParseAppVersion(version string) AppVersion {
+	parts := strings.Split(version, ".")
+	if len(parts) < 1 {
+		return AppVersion{Major: 0, Minor: 0}
+	}
+
+	major, err := strconv.Atoi(parts[0])
+	if err != nil {
+		major = 0
+	}
+
+	minor := 0
+	if len(parts) > 1 {
+		minor, err = strconv.Atoi(parts[1])
+		if err != nil {
+			minor = 0
+		}
+	}
+
+	return AppVersion{Major: major, Minor: minor}
+}
+
+func IsAppVersionEqualOrGreater(versionCurrent, versionCompareTo string) bool {
+	if len(versionCurrent) == 0 || len(versionCompareTo) == 0 {
+		return false
+	}
+	verCurrent := ParseAppVersion(versionCurrent)
+	verCompareTo := ParseAppVersion(versionCompareTo)
+	if verCurrent.Major > verCompareTo.Major {
+		return true
+	} else if verCurrent.Major == verCompareTo.Major && verCurrent.Minor >= verCompareTo.Minor {
+		return true
+	}
+	return false
+}

duplosdk/version_utils.go
@@ -1,47 +1,0 @@
-package duplosdk
-
-import (
-	"strconv"
-	"strings"
-)
-
-type AppVersion struct {
-	Major int
-	Minor int
-}
-
-func ParseAppVersion(version string) AppVersion {
-	parts := strings.Split(version, ".")
-	if len(parts) < 1 {
-		return AppVersion{Major: 0, Minor: 0}
-	}
-
-	major, err := strconv.Atoi(parts[0])
-	if err != nil {
-		major = 0
-	}
-
-	minor := 0
-	if len(parts) > 1 {
-		minor, err = strconv.Atoi(parts[1])
-		if err != nil {
-			minor = 0
-		}
-	}
-
-	return AppVersion{Major: major, Minor: minor}
-}
-
-func IsAppVersionEqualOrGreater(versionCurrent, versionCompareTo string) bool {
-	if len(versionCurrent) == 0 || len(versionCompareTo) == 0 {
-		return false
-	}
-	verCurrent := ParseAppVersion(versionCurrent)
-	verCompareTo := ParseAppVersion(versionCompareTo)
-	if verCurrent.Major > verCompareTo.Major {
-		return true
-	} else if verCurrent.Major == verCompareTo.Major && verCurrent.Minor >= verCompareTo.Minor {
-		return true
-	}
-	return false
-}

commit 38f12066b69a72ff9a227f36d633c43bc24662dd

add ! negation condition back  regression in ed1f996e110fc512a32a7a618acd41fbf01fba09

duplocloud/resource_duplo_ecache_instance.go
@@ -357,11 +357,11 @@ func validateLogDeliveryConfigurations(engineVersion string, configs []duplosdk.
 	for _, config := range configs {
 		switch config.LogType {
 		case "engine-log":
-			if IsAppVersionEqualOrGreater(engineVersion, "6.2.0") {
+			if !IsAppVersionEqualOrGreater(engineVersion, "6.2.0") {
 				return diag.Errorf("log_delivery_configuration with log_type 'engine-log' cannot be used with engine_version '%s'. Please use engine_version '6.2.0' or above.", engineVersion)
 			}
 		case "slow-log":
-			if IsAppVersionEqualOrGreater(engineVersion, "6.0.0") {
+			if !IsAppVersionEqualOrGreater(engineVersion, "6.0.0") {
 				return diag.Errorf("log_delivery_configuration with log_type 'slow-log' cannot be used with engine_version '%s'. Please use engine_version '6.0.0' or above.", engineVersion)
 			}
 		default: