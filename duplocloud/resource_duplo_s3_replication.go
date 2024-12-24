5451871a3d25cce31d992aa132f531e64cb0b366
nikhil <nikhil.nambiar@duplocloud.net>
2024-12-24 13:50:45 +0530

    removed comparison

--- duplocloud/resource_duplo_s3_replication.go
+++ duplocloud/resource_duplo_s3_replication.go
@@ -133,7 +133,8 @@ func resourceS3BucketReplicationRead(ctx context.Context, d *schema.ResourceData
 	if len(idParts) < 3 {
 		return diag.Errorf("resourceS3BucketReplicationRead: Invalid resource (ID: %s)", id)
 	}
-	tenantID, name, ruleName := idParts[0], idParts[1], idParts[2]
+	tenantID, name := idParts[0], idParts[1]
+	//ruleName:=idParts[2]
 
 	c := m.(*duplosdk.Client)
 	duplo, err := getS3BucketReplication(c, tenantID, name)
@@ -148,12 +149,12 @@ func resourceS3BucketReplicationRead(ctx context.Context, d *schema.ResourceData
 	}
 	rp := []map[string]interface{}{}
 	for _, rule := range duplo {
-		fullName := strings.Split(rule["fullname"].(string), "-")
-		n := fullName[len(fullName)-1]
-		if n == ruleName {
-			rp = append(rp, rule)
-			break
-		}
+		//	fullName := strings.Split(rule["fullname"].(string), "-")
+		//	n := fullName[len(fullName)-1]
+		//	if n == ruleName {
+		rp = append(rp, rule)
+		//		break
+		//	}
 	}
 	// Get the object from Duplo
 	d.Set("rules", rp)
