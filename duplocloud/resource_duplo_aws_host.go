1a05ab6eaa064cb1cf81ef581a3a3428e6c5b69c
nikhil <nikhil.nambiar@duplocloud.net>
2025-02-04 14:35:13 +0530

    fixed regression

--- duplocloud/resource_duplo_aws_host.go
+++ duplocloud/resource_duplo_aws_host.go
@@ -3,11 +3,12 @@ package duplocloud
 import (
 	"context"
 	"fmt"
-	"github.com/duplocloud/terraform-provider-duplocloud/duplosdk"
 	"log"
 	"regexp"
 	"strings"
 	"time"
+
+	"github.com/duplocloud/terraform-provider-duplocloud/duplosdk"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/customdiff"
@@ -318,6 +319,11 @@ func diffUserData(ctx context.Context, diff *schema.ResourceDiff, m interface{})
 				return fmt.Errorf("[Error] Error clearing diff for base64_user_data: %s", err)
 			}
 		}
+	}
+	z := diff.Get("zone").(int)
+	agp := diff.Get("agent_platform").(int)
+	if diff.Get("allocated_public_ip").(bool) && z != 0 && agp == 7 {
+		return fmt.Errorf("[Error] Error cannot set zone to %d if allocated_public_ip is set to true", z)
 	}
 
 	return nil
@@ -713,15 +719,15 @@ func nativeHostToState(ctx context.Context, d *schema.ResourceData, duplo *duplo
 		d.Set("metadata", keyValueToState("metadata", duplo.MetaData))
 	}
 	// If a network interface was customized, certain fields are not returned by the backend.
-	if v, ok := d.GetOk("network_interface"); !ok || v == nil || len(v.([]interface{})) == 0 {
-		d.Set("zone", duplo.Zone)
-		d.Set("allocated_public_ip", duplo.AllocatedPublicIP)
-	}
+	//if v, ok := d.GetOk("network_interface"); !ok || v == nil || len(v.([]interface{})) == 0 {
+	d.Set("zone", duplo.Zone)
+	d.Set("allocated_public_ip", duplo.AllocatedPublicIP)
+	//}
 
 	//d.Set("metadata", keyValueToState("metadata", duplo.MetaData))
 	d.Set("volume", flattenNativeHostVolumes(duplo.Volumes))
 	d.Set("network_interface", flattenNativeHostNetworkInterfaces(duplo.NetworkInterfaces))
-	if duplo.IsMinion && duplo.AgentPlatform == 7 {
+	if duplo.IsMinion && duplo.AgentPlatform == 7 && len(d.Get("taints").([]interface{})) > 0 {
 		obj, _ := c.GetMinionForHost(ctx, duplo.TenantID, duplo.InstanceID)
 		if obj != nil && len(obj.Taints) > 0 { //taints only applicable at k8s side
 			d.Set("taints", flattenMinionTaints(obj.Taints))
