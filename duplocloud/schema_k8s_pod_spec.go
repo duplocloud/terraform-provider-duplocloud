4c3d6ee7e81052e8efc02896525033ff0338a649
nikhil <nikhil.nambiar@duplocloud.net>
2025-02-06 13:55:29 +0530

    doc updated

--- duplocloud/schema_k8s_pod_spec.go
+++ duplocloud/schema_k8s_pod_spec.go
@@ -203,7 +203,6 @@ func podSpecFields(isUpdatable, isComputed bool) map[string]*schema.Schema {
 			Type:        schema.TypeList,
 			Description: "ImagePullSecrets is an optional list of references to secrets in the same namespace to use for pulling any of the images used by this PodSpec. If specified, these secrets will be passed to individual puller implementations for them to use. For example, in the case of docker, only DockerConfig type secrets are honored. More info: https://kubernetes.io/docs/concepts/containers/images/#specifying-imagepullsecrets-on-a-pod",
 			Optional:    true,
-			//Computed:    true,
 			Elem: &schema.Resource{
 				Schema: map[string]*schema.Schema{
 					"name": {
