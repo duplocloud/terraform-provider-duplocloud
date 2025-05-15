33647835d6055b7872915aebb06468ded02d7775
nikhil <nikhil.nambiar@duplocloud.net>
2025-05-15 11:51:59 +0530

    under progress

--- duplocloud/resource_duplo_aws_cloudfront_function.go
+++ duplocloud/resource_duplo_aws_cloudfront_function.go
@@ -50,50 +50,60 @@ func resourceAwsCloudfrontFunction() *schema.Resource {
 				Type:        schema.TypeString,
 				Computed:    true,
 			},
+			"publish": {
+				Description: "Whether to publish creation/change as Live CloudFront Function Version",
+				Type:        schema.TypeBool,
+				Default:     true,
+				Optional:    true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
 
-func resourceCloudfrontFunctionCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostic {
+func resourceCloudfrontFunctionCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*duplosdk.Client)
 	tenantID := d.Get("tenant_id").(string)
 
 	// Construct the request
-	request := map[string]interface{}{
-		"Name":    d.Get("name").(string),
-		"Code":    d.Get("code").(string),
-		"Comment": d.Get("comment").(string),
-		"Runtime": d.Get("runtime").(string),
-	}
-
+	req := expandCloudFrontFunction(d)
 	// Call the Duplo API to create the CloudFront function
 	log.Printf("[TRACE] Creating CloudFront function in tenant %s: %v", tenantID, request)
-	err := c.PostAPI(fmt.Sprintf("v3/subscriptions/%s/aws/cloudfrontFunction", tenantID), request, nil)
+	resp, err := c.CreateCloudFrontFunction(tenantID, req)
 	if err != nil {
-		return fmt.Errorf("failed to create CloudFront function: %s", err)
+		return diag.Errorf("failed to create CloudFront function: %s", err)
 	}
-
+	if d.Get("publish").(bool) {
+		log.Printf("[TRACE] Publishing CloudFront function %s in tenant %s", resp.Name, tenantID)
+		err = c.PublishCloudFrontFunction(tenantID, resp.Name)
+		if err != nil {
+			return diag.Errorf("failed to publish CloudFront function: %s", err)
+		}
+	}
 	// Set the resource ID
 	d.SetId(fmt.Sprintf("%s/%s", tenantID, d.Get("name").(string)))
 
 	return resourceCloudfrontFunctionRead(ctx, d, m)
 }
 
-func resourceCloudfrontFunctionRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostic {
+func resourceCloudfrontFunctionRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*duplosdk.Client)
 
-	tenantID, name := parseDuploAwsCloudfrontFunctionId(d.Id())
+	tenantID, name := parseCloudfrontFunctionId(d.Id())
 
 	// Call the Duplo API to get the CloudFront function
 	log.Printf("[TRACE] Reading CloudFront function %s in tenant %s", name, tenantID)
 	function := map[string]interface{}{}
-	err := c.GetAPI(fmt.Sprintf("v3/subscriptions/%s/aws/cloudfrontFunction/%s", tenantID, name), &function)
+	rp, err := c.GetCloudFrontFunction(tenantID, name)
 	if err != nil {
-		if c.IsNotFound(err) {
+		if err.Status() == 404 {
 			d.SetId("")
 			return nil
 		}
-		return fmt.Errorf("failed to read CloudFront function: %s", err)
+		return diag.Errorf("failed to read CloudFront function: %s", err)
 	}
 
 	// Update the state
@@ -107,7 +117,7 @@ func resourceCloudfrontFunctionRead(ctx context.Context, d *schema.ResourceData,
 	return nil
 }
 
-func resourceCloudfrontFunctionUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostic {
+func resourceCloudfrontFunctionUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*duplosdk.Client)
 	tenantID, name := parseCloudfrontFunctionId(d.Id())
 
@@ -129,7 +139,7 @@ func resourceCloudfrontFunctionUpdate(ctx context.Context, d *schema.ResourceDat
 	return resourceCloudfrontFunctionRead(ctx, d, m)
 }
 
-func resourceCloudfrontFunctionDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostic {
+func resourceCloudfrontFunctionDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	c := m.(*duplosdk.Client)
 	tenantID, name := parseDuploAwsCloudfrontFunctionId(d.Id())
 
@@ -149,4 +159,28 @@ func resourceCloudfrontFunctionDelete(ctx context.Context, d *schema.ResourceDat
 func parseCloudfrontFunctionId(id string) (tenantID, name string) {
 	parts := strings.SplitN(id, "/", 2)
 	return parts[0], parts[1]
+}
+
+func expandCloudFrontFunction(d *schema.ResourceData) *duplosdk.DuploCloudFrontFunction {
+	return &DuploCloudFrontFunction{
+		Name:    d.Get("name").(string),
+		Runtime: d.Get("runtime").(string),
+		Code:    d.Get("code").(string),
+		Comment: d.Get("comment").(string),
+	}
+}
+
+func FlattenCloudFrontFunction(d *schema.ResourceData, function *duplosdk.DuploCloudFrontFunction) {
+	if function == nil {
+		return
+	}
+
+	d.Set("name", function.Name)
+	d.Set("runtime", function.Runtime)
+	d.Set("code", function.Code)
+	d.Set("comment", function.Comment)
+	d.Set("status", function.Status)
+	if function.Metadata != nil {
+		d.Set("function_arn", function.Metadata.ARN)
+	}
 }
--- duplosdk/aws_cloudfront_function.go
+++ duplosdk/aws_cloudfront_function.go
@@ -5,23 +5,28 @@ import (
 )
 
 // CloudFrontFunction represents a CloudFront Function in Duplo.
-type CloudFrontFunctionRequest struct {
-	Name    string `json:"Name"`
-	Runtime string `json:"FunctionConfigRuntime"`
-	Code    string `json:"FunctionCode"`
-	Comment string `json:"FunctionConfigComment,omitempty"`
+type DuploCloudFrontFunction struct {
+	Name     string                           `json:"Name"`
+	Runtime  string                           `json:"FunctionConfigRuntime"`
+	Code     string                           `json:"FunctionCode"`
+	Comment  string                           `json:"FunctionConfigComment,omitempty"`
+	Status   string                           `json:"Status,omitempty"`
+	Metadata *DuploCloudFrontFunctionMetadata `json:"FunctionMetadata"`
 }
 
 // CloudFrontFunctionUpdateRequest represents a request to update a CloudFront Function.
-type CloudFrontFunctionUpdateRequest struct {
-	Code  string `json:"code"`
-	Stage string `json:"stage"`
+type DuploCloudFrontFunctionMetadata struct {
+	ARN string `json:"FunctionARN"`
 }
 
 // GetCloudFrontFunction retrieves a CloudFront Function by name.
-func (c *Client) GetCloudFrontFunction(tenantID, name string) (*CloudFrontFunctionRequest, error) {
-	rp := CloudFrontFunctionRequest{}
-	err := c.getAPI(fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction/%s", tenantID, name), &rp)
+func (c *Client) GetCloudFrontFunction(tenantID, name string) (*DuploCloudFrontFunction, ClientError) {
+	rp := DuploCloudFrontFunction{}
+
+	err := c.getAPI(fmt.Sprintf("GetCloudFrontFunction(%s,%s)", tenantID, name),
+		fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction/%s", tenantID, name),
+		&rp)
+
 	if err != nil {
 		return nil, err
 	}
@@ -29,9 +34,13 @@ func (c *Client) GetCloudFrontFunction(tenantID, name string) (*CloudFrontFuncti
 }
 
 // CreateCloudFrontFunction creates a new CloudFront Function.
-func (c *Client) CreateCloudFrontFunction(tenantID string, rq *CloudFrontFunctionCreateRequest) (*CloudFrontFunction, error) {
-	rp := CloudFrontFunction{}
-	err := c.postAPI(fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction", tenantID), rq, &rp)
+func (c *Client) CreateCloudFrontFunction(tenantID string, rq *DuploCloudFrontFunction) (*DuploCloudFrontFunction, ClientError) {
+	rp := DuploCloudFrontFunction{}
+	err := c.postAPI(fmt.Sprintf("CreateCloudFrontFunction(%s,%s)", tenantID, rq.Name),
+		fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction", tenantID),
+		rq,
+		&rp)
+
 	if err != nil {
 		return nil, err
 	}
@@ -39,16 +48,28 @@ func (c *Client) CreateCloudFrontFunction(tenantID string, rq *CloudFrontFunctio
 }
 
 // UpdateCloudFrontFunction updates an existing CloudFront Function.
-func (c *Client) UpdateCloudFrontFunction(tenantID, name string, rq *CloudFrontFunctionUpdateRequest) (*CloudFrontFunction, error) {
-	rp := CloudFrontFunction{}
-	err := c.putAPI(fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction/%s", tenantID, name), rq, &rp)
-	if err != nil {
-		return nil, err
-	}
-	return &rp, nil
-}
+//func (c *Client) UpdateCloudFrontFunction(tenantID, name string, rq *CloudFrontFunctionUpdateRequest) (*CloudFrontFunction, ClientError) {
+//	rp := CloudFrontFunction{}
+//	err := c.putAPI(fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction/%s", tenantID, name), rq, &rp)
+//	if err != nil {
+//		return nil, err
+//	}
+//	return &rp, nil
+//}
 
 // DeleteCloudFrontFunction deletes a CloudFront Function by name.
-func (c *Client) DeleteCloudFrontFunction(tenantID, name string) error {
-	return c.deleteAPI(fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction/%s", tenantID, name), nil)
+//func (c *Client) DeleteCloudFrontFunction(tenantID, name string) ClientError {
+//	return c.deleteAPI(fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction/%s", tenantID, name), nil)
+//}
+
+// PublishCloudFrontFunction publishes a CloudFront Function by name.
+func (c *Client) PublishCloudFrontFunction(tenantID, name string) ClientError {
+	rq := map[string]string{
+		"Name": name,
+	}
+	var rp interface{}
+	return c.putAPI(fmt.Sprintf("PublishCloudFrontFunction(%s,%s)", tenantID, name),
+		fmt.Sprintf("v3/subscriptions/%s/cloudfrontFunction/%s/publish", tenantID, name),
+		rq,
+		&rp)
 }
