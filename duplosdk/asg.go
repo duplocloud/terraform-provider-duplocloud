693306c8ea9b3970030ce282218b147d146b6116
nikhil <nikhil.nambiar@duplocloud.net>
2025-06-02 19:36:03 +0530

    backward compatibility:

--- duplosdk/asg.go
+++ duplosdk/asg.go
@@ -39,7 +39,7 @@ type DuploAsgProfile struct {
 	EnabledMetrics      *[]string                          `json:"EnabledMetrics,omitempty"`
 	ExtraNodeLabels     *[]DuploKeyStringValue             `json:"ExtraNodeLabels,omitempty"`
 	Taints              *[]DuploTaints                     `json:"Taints,omitempty"`
-	Created             bool                               `json:"Created"`
+	Created             *bool                              `json:"Created,omitempty"`
 }
 
 type DuploAsgProfileDeleteReq struct {
