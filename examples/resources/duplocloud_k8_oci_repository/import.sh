9f660636cbe4019368a288d1b97b98585d53d25e
nikhil <nikhil.nambiar@duplocloud.net>
2025-06-25 15:30:27 +0530

    support for oci

---
+++ examples/resources/duplocloud_k8_oci_repository/import.sh
@@ -0,0 +1,5 @@
+# Example: Importing an existing helm repository
+#  - *TENANT_ID* is the tenant GUID
+#  - *NAME* is the oci repository name
+#
+terraform import duplocloud_k8_oci_repository.repo *TENANT_ID*/oci-repository/*NAME*
