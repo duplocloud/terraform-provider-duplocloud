fbb65868b17cd94c1f0af48fed47e02ad3c0d864
nikhil <nikhil.nambiar@duplocloud.net>
2025-09-17 19:24:12 +0530

    support for block_device_mapping

--- examples/resources/duplocloud_aws_launch_template/resource.tf
+++ examples/resources/duplocloud_aws_launch_template/resource.tf
@@ -11,3 +11,23 @@ resource "duplocloud_aws_launch_template" "lt" {
   version_description = "launch template description"
   ami                 = "ami-123test"
 }
+
+resource "duplocloud_aws_launch_template" "name" {
+  tenant_id           = duplocloud_tenant.myapp.tenant_id
+  instance_type       = "t3a.small"
+  name                = "launch-template-name"
+  version_description = "launch template block device mapping"
+  version             = "2"
+  block_device_mapping {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = 30
+      volume_type           = "gp3"
+      delete_on_termination = true
+      encrypted             = false
+      iops                  = 3000
+    }
+    virtual_name = "ephemeral1"
+  }
+
+}
