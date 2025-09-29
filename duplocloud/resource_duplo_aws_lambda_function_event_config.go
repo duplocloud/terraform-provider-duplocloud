a7c2efd912352eca6615ee8db7353e3eb76b1be5
nikhil <nikhil.nambiar@duplocloud.net>
2025-09-29 12:23:27 +0530

    added default value to max retry attempt

--- duplocloud/resource_duplo_aws_lambda_function_event_config.go
+++ duplocloud/resource_duplo_aws_lambda_function_event_config.go
@@ -37,6 +37,7 @@ func awsLambdaFunctionEventInvokeConfigSchema() map[string]*schema.Schema {
 			Description:  "Maximum number of attempts a Lambda function may retry in case of error",
 			Type:         schema.TypeInt,
 			Optional:     true,
+			Default:      2,
 			ValidateFunc: validation.IntBetween(0, 2),
 		},
 		"max_event_age_in_seconds": {
