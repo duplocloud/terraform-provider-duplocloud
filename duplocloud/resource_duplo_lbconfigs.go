847695a88e36efad130dd33c9120ce527a58d62e
nikhil <nikhil.nambiar@duplocloud.net>
2025-11-10 21:29:39 +0530

    support for nlb elastic ips

--- duplocloud/resource_duplo_lbconfigs.go
+++ duplocloud/resource_duplo_lbconfigs.go
@@ -227,6 +227,14 @@ func duploLbConfigSchema() map[string]*schema.Schema {
 				},
 			},
 		},
+		"eip_allocations": {
+			Description: "Allocate Elastic IP to load balancer, which is configured under plan configuration.\n\nNote: This field can only be set for non internal lbtype NLB(6)",
+			Type:        schema.TypeList,
+			Optional:    true,
+			Elem: &schema.Schema{
+				Type: schema.TypeString,
+			},
+		},
 	}
 }
 
@@ -441,6 +449,12 @@ func resourceDuploServiceLBConfigsCreateOrUpdate(ctx context.Context, d *schema.
 				if item.IsInternal {
 					item.AllowGlobalAccess = lbc["allow_global_access"].(bool)
 				}
+				if eips, ok := lbc["eip_allocations"]; ok && len(eips.([]interface{})) > 0 {
+					item.UseEIPFromPool = true
+					for _, v := range eips.([]interface{}) {
+						item.AllocationIds = append(item.AllocationIds, v.(string))
+					}
+				}
 				list = append(list, item)
 			}
 		}
@@ -631,7 +645,13 @@ func flattenDuploServiceLbConfiguration(lb *duplosdk.DuploLbConfiguration) map[s
 		m["backend_protocol_version"] = "HTTP1"
 
 	}
-
+	if len(lb.EIPAllocationIds) > 0 {
+		eips := []interface{}{}
+		for _, ips := range lb.EIPAllocationIds {
+			eips = append(eips, ips)
+		}
+		m["eip_allocations"] = eips
+	}
 	log.Printf("[DEBUG] flattenDuploServiceLbConfiguration... End")
 	return m
 }
@@ -645,7 +665,7 @@ func validateLBConfigParameters(ctx context.Context, diff *schema.ResourceDiff, 
 		p := strings.ToLower(pr)
 		b := m["backend_protocol_version"].(string)
 		bp := strings.ToLower(b)
-
+		eips := m["eip_allocations"].([]interface{})
 		lb, ok := m["lb_type"].(int)
 		if ok && lb != 1 && bp != "" && bp != "http1" {
 			return fmt.Errorf("backend_protocol_version field is available only for ALB for others load balancer type use protocol")
@@ -676,6 +696,11 @@ func validateLBConfigParameters(ctx context.Context, diff *schema.ResourceDiff, 
 			if hm["timeout"].(int) >= hm["interval"].(int) {
 				return fmt.Errorf("health check timeout must be less than health check interval")
 			}
+		}
+		if lb != 6 && len(eips) > 0 {
+			return fmt.Errorf("eip_allocations can only be set for NLB")
+		} else if lb == 6 && m["is_internal"].(bool) {
+			return fmt.Errorf("eip_allocations can only be set for public NLB")
 		}
 	}
 	return nil
