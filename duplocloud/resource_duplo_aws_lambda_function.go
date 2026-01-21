f750d24d923fdf9c1a01f0a44cb2a229ab469af2
amaechiabuah <amaechi.abuah@duplocloud.net>
2026-01-21 16:40:25 +0100

    Added python3.14 runtime support to duplocloud_aws_lambda_function

--- duplocloud/resource_duplo_aws_lambda_function.go
+++ duplocloud/resource_duplo_aws_lambda_function.go
@@ -144,7 +144,7 @@ func awsLambdaFunctionSchema() map[string]*schema.Schema {
 			ValidateFunc: validation.StringInSlice([]string{
 				"nodejs", "nodejs4.3", "nodejs6.10", "nodejs8.10", "nodejs10.x", "nodejs12.x", "nodejs14.x", "nodejs16.x", "nodejs18.x", "nodejs20.x", "nodejs22.x",
 				"java8", "java8.al2", "java11", "java17", "java21",
-				"python2.7", "python3.6", "python3.7", "python3.8", "python3.9", "python3.10", "python3.11", "python3.12", "python3.13",
+				"python2.7", "python3.6", "python3.7", "python3.8", "python3.9", "python3.10", "python3.11", "python3.12", "python3.13", "python3.14",
 				"dotnetcore1.0", "dotnetcore2.0", "dotnetcore2.1", "dotnetcore3.1",
 				"dotnet6", "dotnet7", "dotnet8",
 				"nodejs4.3-edge",
