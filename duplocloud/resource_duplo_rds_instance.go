11ae00d11751e55a3fed140b3b3a964c96edb0aa
nikhil <nikhil.nambiar@duplocloud.net>
2026-02-05 18:02:24 +0530

    removed commented diffsupp function

--- duplocloud/resource_duplo_rds_instance.go
+++ duplocloud/resource_duplo_rds_instance.go
@@ -292,7 +292,6 @@ func rdsInstanceSchema() map[string]*schema.Schema {
 			Type:        schema.TypeBool,
 			Optional:    true,
 			Computed:    true,
-			//	DiffSuppressFunc: diffSuppressWhenNotCreating,
 		},
 		"enhanced_monitoring": {
 			Description:  "Interval to capture metrics in real time for the operating system (OS) that your Amazon RDS DB instance runs on.",
