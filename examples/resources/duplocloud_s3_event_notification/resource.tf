029131aad2a37f18b0cbf9854a4defbb888bc5bb
nikhil <nikhil.nambiar@duplocloud.net>
2026-02-06 20:54:17 +0530

    added doc example

--- examples/resources/duplocloud_s3_event_notification/resource.tf
+++ examples/resources/duplocloud_s3_event_notification/resource.tf
@@ -8,7 +8,7 @@ resource "duplocloud_tenant" "myapp" {
 
 resource "duplocloud_s3_event_notification" "event" {
   tenant_id   = duplocloud_tenant.myapp.tenant_id
-  bucket_name = "duploservices-<tenantname>-<bucket>"
+  bucket_name = "duploservices-<tenantname>-<bucket1>"
   event {
     destination_type = "lambda"
     destination_arn  = "<arn-of-destination-type>"
@@ -22,7 +22,7 @@ resource "duplocloud_s3_event_notification" "event" {
 
 resource "duplocloud_s3_event_notification" "event" {
   tenant_id   = duplocloud_tenant.myapp.tenant_id
-  bucket_name = "duploservices-<tenantname>-<bucket>"
+  bucket_name = "duploservices-<tenantname>-<bucket2>"
   event {
     destination_type = "sqs"
     destination_arn  = "<arn-of-destination-type>"
@@ -36,12 +36,60 @@ resource "duplocloud_s3_event_notification" "event" {
 
 resource "duplocloud_s3_event_notification" "event" {
   tenant_id   = duplocloud_tenant.myapp.tenant_id
-  bucket_name = "duploservices-<tenantname>-<bucket>"
+  bucket_name = "duploservices-<tenantname>-<bucket3>"
   event {
     destination_type = "sns"
     destination_arn  = "<arn-of-destination-type>"
     event_types      = ["s3:ObjectCreated:Put", "s3:ObjectRemoved:DeleteMarkerCreated"]
   }
   enable_event_bridge = true
+
+}
+
+# Simple Example 4: Multiple event type with different destination for an S3 bucket
+
+resource "duplocloud_s3_event_notification" "se" {
+  tenant_id           = "b4315a73-6455-4e99-8e26-7a771018cc1e"
+  bucket_name         = "duploservices-sep8-eventtests-182680712604"
+  enable_event_bridge = false
+  event {
+    destination_type = "lambda"
+    event_types      = ["s3:ObjectCreated:Put"]
+    destination_arn  = "<arn-of-destination-type>"
+  }
+  event {
+    destination_type = "sns"
+    destination_arn  = "<arn-of-destination-type>"
+    event_types      = ["s3:ObjectCreated:CompleteMultipartUpload"]
+  }
+  event {
+    destination_type = "sqs"
+    event_types      = ["s3:ObjectCreated:Post", "s3:ObjectRemoved:DeleteMarkerCreated"]
+    destination_arn  = "<arn-of-destination-type>"
+  }
+
+}
+
+# Simple Example 5: Multiple event type with same destination for an S3 bucket
+
+resource "duplocloud_s3_event_notification" "se" {
+  tenant_id           = "b4315a73-6455-4e99-8e26-7a771018cc1e"
+  bucket_name         = "duploservices-sep8-eventtests-182680712604"
+  enable_event_bridge = false
+  event {
+    destination_type = "sqs"
+    event_types      = ["s3:ObjectCreated:Put"]
+    destination_arn  = "<arn-of-destination-type>"
+  }
+  event {
+    destination_type = "sqs"
+    destination_arn  = "<arn-of-destination-type>"
+    event_types      = ["s3:ObjectCreated:CompleteMultipartUpload"]
+  }
+  event {
+    destination_type = "sqs"
+    event_types      = ["s3:ObjectCreated:Post", "s3:ObjectRemoved:DeleteMarkerCreated"]
+    destination_arn  = "<arn-of-destination-type>"
+  }
 
 }
