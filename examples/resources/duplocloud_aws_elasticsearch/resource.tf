0a6642050cd52a079164ac1c07c1bdd284c33494
nikhil <nikhil.nambiar@duplocloud.net>
2026-03-05 20:31:29 +0530

    document update and diff fix

--- examples/resources/duplocloud_aws_elasticsearch/resource.tf
+++ examples/resources/duplocloud_aws_elasticsearch/resource.tf
@@ -17,3 +17,34 @@ resource "duplocloud_aws_elasticsearch" "es-doc" {
   require_ssl                    = true
   use_latest_tls_cipher          = true
 }
+
+
+resource "duplocloud_aws_elasticsearch" "sample" {
+  tenant_id             = duplocloud_tenant.myapp.tenant_id
+  name                  = "es-sample"
+  selected_zone         = 1
+  elasticsearch_version = "OpenSearch_2.3"
+  ebs_options {
+    ebs_enabled = true
+    volume_type = "gp2"
+    volume_size = 10
+  }
+}
+
+resource "duplocloud_aws_elasticsearch" "sample" {
+  tenant_id             = duplocloud_tenant.myapp.tenant_id
+  name                  = "es-sample2"
+  selected_zone         = 1
+  elasticsearch_version = "OpenSearch_2.3"
+  cluster_config {
+    instance_type          = "t3.small.search"
+    dedicated_master_type  = "t3.small.search"
+    dedicated_master_count = 3
+  }
+  ebs_options {
+    ebs_enabled = true
+    iops        = 0
+    volume_size = 10
+    volume_type = "gp2"
+  }
+}
