53c800cc3a6182cc92abfa0462873f7d608fbb61
amaechiabuah <amaechi.abuah@duplocloud.net>
2026-05-28 15:58:54 +0100

    DUPLO-31711 TF: validate expiration_policy.ttl to reject malformed durations at plan time

--- duplocloud/resource_duplo_gcp_pubsub_subscription.go
+++ duplocloud/resource_duplo_gcp_pubsub_subscription.go
@@ -291,9 +291,10 @@ func gcpPubSubSubscriptionSchema() map[string]*schema.Schema {
 			Elem: &schema.Resource{
 				Schema: map[string]*schema.Schema{
 					"ttl": {
-						Description: "Specifies the 'time-to-live' duration for an associated resource. The resource expires if it is not active for a period of ttl. If ttl is empty string, the associated resource never expires.  A duration in seconds with up to nine fractional digits, terminated by 's'. Example - '3.5s'.",
-						Type:        schema.TypeString,
-						Required:    true,
+						Description:  "Specifies the 'time-to-live' duration for an associated resource. The resource expires if it is not active for a period of ttl. If ttl is empty string, the associated resource never expires.  A duration in seconds with up to nine fractional digits, terminated by 's'. Example - '3.5s'.",
+						Type:         schema.TypeString,
+						Required:     true,
+						ValidateFunc: validateDurationBetween(0, 365*24*time.Hour, 9),
 					},
 				},
 			},
