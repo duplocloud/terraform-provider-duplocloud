36b5f68ed09a29c60c9c1dd4dd45e08cc3609b37
nikhil <nikhil.nambiar@duplocloud.net>
2026-07-10 10:48:16 +0530

    flatten fix and doc update

--- duplocloud/data_source_duplo_service.go
+++ duplocloud/data_source_duplo_service.go
@@ -217,14 +217,13 @@ func flattenDuploService(d *schema.ResourceData, duplo *duplosdk.DuploReplicatio
 			d.Set("init_container_docker_image", initContainerImages)
 		}
 	}
-	if duplo.Template.Cloud == 2 {
-		if duplo.K8SWorkerOs != nil && *duplo.K8SWorkerOs == 1 {
-			d.Set("k8s_worker_os", "Windows")
-		} else if duplo.K8SWorkerOs == nil {
-			d.Set("k8s_worker_os", "Linux")
-		} else if *duplo.K8SWorkerOs == 0 {
-			d.Set("k8s_worker_os", "Linux")
-		}
+
+	// k8s_worker_os applies to all clouds. Default to Linux when the backend
+	// does not return a value (nil or 0); 1 indicates Windows.
+	if duplo.K8SWorkerOs != nil && *duplo.K8SWorkerOs == 1 {
+		d.Set("k8s_worker_os", "Windows")
+	} else {
+		d.Set("k8s_worker_os", "Linux")
 	}
 	d.Set("app_name", duplo.AppName)
 }
