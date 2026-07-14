2159afa13230fb840a6c9be158501d972e9e5029
amaechiabuah <amaechi.abuah@duplocloud.net>
2026-07-14 16:36:42 +0100

    DUPLO-43564 TF: inherit multi_az_enabled from primary on GDS secondary associate

--- duplosdk/ecache_instance.go
+++ duplosdk/ecache_instance.go
@@ -395,6 +395,9 @@ type DuploEcacheReplicationGroup struct {
 	ReplicationGroupId       string `json:"ReplicationGroupId"`
 	KmsKeyId                 string `json:"KmsKeyId,omitempty"`
 	AuthToken                string `json:"AuthToken,omitempty"`
+	// MultiAZEnabled is inherited from the primary at create time so the secondary mirrors
+	// the primary's Multi-AZ setting (the user never sets it on the secondary resource).
+	MultiAZEnabled bool `json:"MultiAZEnabled"`
 }
 
 func (c *Client) DuploEcacheReplicationGroupCreate(tenantID string, rq *DuploEcacheReplicationGroup) (*DuploEcacheGlobalDatastoreResponse, ClientError) {
